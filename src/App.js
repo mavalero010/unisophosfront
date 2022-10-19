499cf7585b773fe9c741a69bc072ac4c420cf7ce
Michael Andres Valero Polo <mavalero@uninorte.edu.co>
2022-10-19 15:23:18 -0500

    Primer Frame

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
+import log from './recursos/imagenes/UniSophosLogo.png';
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
+import { FiArrowRight } from "react-icons/fi";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label id="InitLabel1">UNIVERSIDAD SOPHOS SOLUTIONS</label>
+        <img src={log} className="App-logo" alt="logo" />
+        <div>
+        <label id="InitLabel2">
+          
+          INGRESE NIT DE UNIVERSIDAD PARA INICIAR SESIÓN
+          </label>
+          <button id="ButtonInitLabel"><FiArrowRight /></button>
+        </div>
+        <form>
+        <label for="NIT" >NIT:</label>
+        <input type="text" id="InputAreaInitLabel2" ></input>
+        </form>
+
       </header>
     </div>
   );
