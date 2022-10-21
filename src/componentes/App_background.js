7190907422869bccf1c2f07f8f5e94aa1f61a272
mavalero010 <37789587+mavalero010@users.noreply.github.com>
2022-10-20 22:52:21 -0500

    Update App_background.js

--- src/componentes/App_background.js
+++ src/componentes/App_background.js
@@ -4,14 +4,31 @@ function App_Back_ground(){
 return (<div>
     <header className="orange_ceiling"></header>
     <div align="center" className='table'>
-       <div >
-       <label id="pestaña">CURSO</label>
-       <label id="pestaña">PROFESOR</label>
-       <label id="pestaña">ESTUDIANTE</label>
-       <div id='tablagris'>
-        HOLA
-        </div>      
-       </div>
+    <div class="tabs">
+<div class="tab-container">
+  <div id="tab3" class="tab"> 
+    <a href="#tab3">Curso</a>
+    <div class="tab-content">
+      <h2>Titulo 3</h2>
+      <p>Lorem ipsum ...</p>
+    </div>
+  </div>
+  <div id="tab2" class="tab">
+    <a href="#tab2">Profesor</a>
+    <div class="tab-content">
+      <h2>Titulo 2</h2>
+      <p>Lorem ipsum ...</p>
+    </div>
+  </div> 
+  <div id="tab1" class="tab">
+    <a href="#tab1">Estudiante</a>
+    <div class="tab-content">
+      <h2>TituloTituloTituloTituloTituloTituloTituloTituloTituloTituloTituloTituloTituloTituloTituloTituloTitulo 1</h2>
+      <p>Lorem ipsum ...</p>
+    </div> 
+  </div> 
+</div>
+</div>
     </div>
 </div>);
 }
